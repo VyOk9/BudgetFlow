38ef2133c3be2ee23ab7775c0edc196280f54ad1
lhay9 <lylian.hay@epitech.eu>
2025-07-10 17:16:27 +0200

    init(Backend): init the project backend with Nest

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from '../prisma/prisma.module';
+import { ExpenseModule } from './expense/expense.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, ExpenseModule],
   controllers: [AppController],
   providers: [AppService],
 })
